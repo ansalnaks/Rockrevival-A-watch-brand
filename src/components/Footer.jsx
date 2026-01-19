7682d050872044555924509869f6bd0632d7b582
Ansalna <ansalnaksupm@gmail.com>
2026-01-19 20:09:22 +0530

    footer changed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 
 import { Container } from 'react-bootstrap'
-import { Facebook, Instagram, Link, Twitter, Youtube } from 'react-bootstrap-icons'
+import { Facebook, Instagram,Twitter, Youtube } from 'react-bootstrap-icons'
+import { Link } from 'react-router-dom'
 
 function Footer() {
     return (
@@ -13,28 +14,28 @@ function Footer() {
                 <div className='footer-links'>
                     <div>
                         <h5>Collections</h5>
-                        <Link to="/men">Men</Link>
-                        <Link to="/women">Women</Link>
-                        <Link to="/ls">Limited Edition</Link>
+                        <Link to="/men" className="footer-link">Men</Link>
+                        <Link to="/women" className="footer-link">Women</Link>
+                        <Link to="/ls" className="footer-link">Limited Edition</Link>
                     </div>
                     <div>
                         <h5>Company</h5>
-                        <Link to="/about">About us</Link>
-                        <Link to="/her">Heritage</Link>
-                        <Link to="/acreer">Careers</Link>
+                        <Link to="/about" className="footer-link">About us</Link>
+                        <Link to="/her" className="footer-link">Heritage</Link>
+                        <Link to="/acreer" className="footer-link">Careers</Link>
                     </div>
                     <div>
                         <h5>Legal</h5>
-                        <Link to="/poli">Privacy Notice</Link>
-                        <Link to="/cook">Cookie Notice</Link>
-                        <Link to="/sett">Cookie Settings</Link>
-                        <Link to="/tou">Terms of use</Link>
+                        <Link to="/poli" className="footer-link">Privacy Notice</Link>
+                        <Link to="/cook" className="footer-link">Cookie Notice</Link>
+                        <Link to="/sett" className="footer-link">Cookie Settings</Link>
+                        <Link to="/tou" className="footer-link">Terms of use</Link>
                     </div>
                     <div>
                         <h5>Support</h5>
-                        <Link to="/contact">Contact</Link>
-                        <Link to="/warr">Warrenty</Link>
-                        <Link to="/service">Service Centers</Link>
+                        <Link to="/contact" className="footer-link">Contact</Link>
+                        <Link to="/warr" className="footer-link">Warrenty</Link>
+                        <Link to="/service" className="footer-link">Service Centers</Link>
                     </div>
                     <div className="rr-newsletter">
                         <h2>Join the RR Family</h2>
